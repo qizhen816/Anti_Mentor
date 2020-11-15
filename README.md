dd64b482bf27c0372c0b24777a69b3e69fe1a104
Shepherd <qizhen816@163.com>
2020-11-15 22:06:02 +0800

    版本0.0.1
    版本0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Anti_Mentor
 This is an honorable instrunction of automatically warning yourself when working fish in lab.
+
+#如何在办公室反监控小米摄像头快乐摸鱼  
+主要思路是模拟浏览器登录路由器管理网页→进入监控流量上传下载的子网页→找到摄像头ID→监控上传流量→发现异常报警，打开学术页面
+
+*需要满足的条件*：TP-Link路由器，否则需要自行修改页面逻辑
+
+**我爬虫太菜了，不会写路由器的加密逻辑，现在需要人工输入加密后的密码，将会在最近更新自动登录功能**
+
+1. 登录路由器管理页面（一般是192.168.0.1或者192.168.1.1），F12开启开发者模式，定位到密码字段，输入密码，获得加密后的字符串
+
+2. 修改[webcam_alert.py](webcam_alert.py)中的关键参数，详情看代码备注
+
+3. 测试代码 `python webcam_alert.py`
+
+#TODO： 
+自动化登录（填充账号密码），使用selenium模拟浏览器实现  
+模拟浏览器的安装参考[https://www.cnblogs.com/lfri/p/10542797.html](https://www.cnblogs.com/lfri/p/10542797.html)
--- webcam_alert.py
+++ webcam_alert.py
@@ -2,53 +2,31 @@ import json
 import re
 import time
 import requests
-import base64
-import webbrowser
 import random
+import webbrowser
 from bs4 import BeautifulSoup
 
-MIMA = 'tang1981'
-dev_name = "SXT"
+# 路由器管理页登录ip
 url = 'http://192.168.1.1'
+# 路由器设备控制页面ip
 url_s = 'http://192.168.1.1/cgi-bin#/pc/deviceManage'
-driver_path = "chromedriver.exe"
+# 加密后的密码
+pwd = "o1T3EXQ%2FZ44WZebcXbfHb%2BVbzmvM3WqH3EmJwJ%2FBh3M%2BISZSeKBe6yXMf6OI4oOglglSghzUxYtSi%2Fmvfy3f%2BOVkgJdTlaIKocS0pU8cyFZDWZuikoQoaK8Kw7XWxd9u2v7PGdEpVP1ajapolS%2BzRFFQTgpGBEIPid3hKV8MVic%3D"
+# 路由器设备控制页面的摄像头名称，可以自行修改
+dev_name = "SXT"
+# 上传速度阈值 KB/s
 limit = 10
-
+# 学术页面，在检测到摄像头打开时开启，随机抽取列表中的一个
 web = ['http://www.baidu.com',
        'https://github.com/YangZhongy/TrafficLights_control_pytorch_rl',
        'https://zhuanlan.zhihu.com/p/21421729',
        'https://www.coursera.org/browse?source=deprecated_spark_cdp',
        'https://blog.csdn.net/yeqiang19910412/article/details/76468407']
 
-headers1 = {
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-        'Content-Length': '39',
-        'Content-Type': 'application/json; charset=UTF-8',
-        'Host': '192.168.1.1',
-        'Origin': 'http://192.168.1.1',
-        'Proxy-Connection': 'keep-alive',
-        'Referer': 'http://192.168.1.1/',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
-        'X-Requested-With': 'XMLHttpRequest',
-    }
-
-payload = json.dumps({
-    "method": "do",
-    "login":{"encrypt_type": 1,
-             "password":
-"o1T3EXQ%2FZ44WZebcXbfHb%2BVbzmvM3WqH3EmJwJ%2FBh3M%2BISZSeKBe6yXMf6OI4oOglglSghzUxYtSi%2Fmvfy3f%2BOVkgJdTlaIKocS0pU8cyFZDWZuikoQoaK8Kw7XWxd9u2v7PGdEpVP1ajapolS%2BzRFFQTgpGBEIPid3hKV8MVic%3D"
-
-             }})
-
-url_dhcpclient = 'http://192.168.1.1'
-
-def login_old():
-    return requests.post(url_dhcpclient,data=payload,headers=headers1)
-
-def sp(int):
-    time.sleep(int)
+''' TODO 使用selenium模拟登陆
+pwd = '你的路由器密码'
+# selenium设置的driver名称
+driver_path = "chromedriver.exe"
 
 def login():
     from selenium.webdriver.chrome.options import Options
@@ -61,7 +39,7 @@ def login():
     #
     browser.get(url_dhcpclient)
     sp(5)
-    browser.find_element_by_id('lgPwd').send_keys('tang1981')
+    browser.find_element_by_id('lgPwd').send_keys(MIMA)
     browser.find_element_by_id('loginSub').click()
     sp(2)
     browser.get(url_dhcpclient)
@@ -69,12 +47,42 @@ def login():
     browser.find_element_by_id('routeMgtMbtn').click()
     sp(3)
     return browser
+    
+'''
 
+
+headers1 = {
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+    'Content-Length': '39',
+    'Content-Type': 'application/json; charset=UTF-8',
+    'Host': url[6:],
+    'Origin': url,
+    'Proxy-Connection': 'keep-alive',
+    'Referer': url,
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest',
+}
+
+payload = json.dumps({
+    "method": "do",
+    "login": {"encrypt_type": 1,
+              "password": pwd
+              }})
+
+
+def login_old():
+    return requests.post(url, data=payload, headers=headers1)
+
+
+def sp(int):
+    time.sleep(int)
 
 def get_all_host(r):
     # stok = r.get('stok')
-    stok = re.findall('stok":".*?"',r, re.S)[0][7:-1]
-    payload = json.dumps({"hosts_info":{"table":"host_info"},"method":"get"})
+    stok = re.findall('stok":".*?"', r, re.S)[0][7:-1]
+    payload = json.dumps({"hosts_info": {"table": "host_info"}, "method": "get"})
     headers = {
         'Accept': 'application/json, text/javascript, */*; q=0.01',
         'Accept-Encoding': 'gzip, deflate',
@@ -82,20 +90,18 @@ def get_all_host(r):
         'Content-Length': '54',
         'Content-Type': 'application/json; charset=UTF-8',
         'Host': '192.168.1.1',
-        'Origin': 'http://192.168.1.1',
+        'Origin': url,
         'Proxy-Connection': 'keep-alive',
-        'Referer': 'http://192.168.1.1/',
+        'Referer': url,
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
         'X-Requested-With': 'XMLHttpRequest',
     }
-    url = 'http://192.168.1.1/stok='+stok+'/ds'
-    response = requests.post(url, data=payload, headers=headers)
+    url_ = url + '/stok=' + stok + '/ds'
+    response = requests.post(url_, data=payload, headers=headers)
     return response.text
 
-#78-11-DC-D6-9F-5F
-#48-8A-D2-49-40-EA
 
-def getUpSpeedBS(browser,safe):
+def getUpSpeedBS(browser, safe):
     soup = BeautifulSoup(browser.page_source, "html.parser")
     items = soup.select('#eptMngList')[0].find_all('div', {"class": "eptCon"})
     for item in items:
@@ -103,10 +109,10 @@ def getUpSpeedBS(browser,safe):
             pp = item.find('p', {'class': 'vs'}).get_text()
             su = pp.split("上行")[1].split("下行")[0]
             sd = pp.split("上行")[1].split("下行")[1]
-            print(time.asctime(time.localtime(time.time())),'上传',su,'下载',sd)
+            print(time.asctime(time.localtime(time.time())), '上传', su, '下载', sd)
             if su[-4] == 'M' or sd[-4] == 'M':
                 if safe == True:
-                    webbrowser.open(web[int(random.uniform(0,4))])
+                    webbrowser.open(web[int(random.uniform(0, 4))])
                     safe = False
             else:
                 if (su[-4] == 'K' and int(su.split('K')[0]) >= limit) or (
@@ -118,16 +124,16 @@ def getUpSpeedBS(browser,safe):
                     safe = True
     return safe
 
-def getUpSpeed(rr,safe):
 
-    hosts = re.findall('mac":.*?hostname',rr, re.S)
+def getUpSpeed(rr, safe):
+    hosts = re.findall('mac":.*?hostname', rr, re.S)
     for j in hosts:
-        if j.find('78-11-DC-D6-9F-5F')!=-1:
-            speed = re.findall('"up_speed":"\d+"',j)[0]
-            speed = int(re.findall(r'\d+',speed)[0])/1024
-            print(time.asctime(time.localtime(time.time()) ),speed,'kB/s')
+        if j.find('78-11-DC-D6-9F-5F') != -1:
+            speed = re.findall('"up_speed":"\d+"', j)[0]
+            speed = int(re.findall(r'\d+', speed)[0]) / 1024
+            print(time.asctime(time.localtime(time.time())), speed, 'kB/s')
             if speed > 10 and safe == True:
-                webbrowser.open(web[int(random.uniform(0,4))])
+                webbrowser.open(web[int(random.uniform(0, 4))])
                 safe = False
             if speed < 10 and safe == False:
                 safe = True
@@ -138,13 +144,12 @@ def newLog(t):
     page = login_old()
     print(page)
     safe = True
+    # 这里的50000是个阈值，程序开启的时长
     while time.time() - t < 50000 or safe == False:
         rr = get_all_host(page.text)
-        safe = getUpSpeed(rr,safe)
+        safe = getUpSpeed(rr, safe)
         time.sleep(0.1)
 
-def main():
+
+if __name__ == '__main__':
     newLog(time.time())
-
-
-main()
